68a15ebaa02d0020e460842d400c8e1d871993a9
Othniel <wasoker2006@gmail.com>
2026-04-19 11:55:11 +0000

    chore: initial package structure

---
+++ packages/OthnielkitCrud/src/OthnielkitCrudServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Othnielkit\Crud;
+
+use Illuminate\Support\ServiceProvider;
+
+class OthnielkitCrudServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Les commandes seront enregistrées ici plus tard
+    }
+
+    public function register()
+    {
+        //
+    }
+}
